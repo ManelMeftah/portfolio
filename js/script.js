20f07bf71309960baadc94506559dd6bda9e3c97
Manel <61079752+ManelMeftah@users.noreply.github.com>
2022-04-06 11:50:45 +0200

    Update

--- js/script.js
+++ js/script.js
@@ -11,3 +11,29 @@ $(document).ready(function(){
         }
    
 });
+
+
+$(window).on("load",function() {
+  /*  var boxDraw = $(".fade");
+    var boxDrawTop = boxDraw.offset().top;
+    var windowHeight = $(window).height();
+  */  
+    
+          $(window).scroll(function() {
+  //  var scroll = $(window).scrollTop();
+            var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+            $(".fade").each(function() {
+              /* Check the location of each desired element */
+              //var objectBottom = $(this).offset().top + $(this).outerHeight();
+var objectBottom = $(this).offset().top;
+              /* If the element is completely within bounds of the window, fade it in */
+              if (objectBottom < windowBottom)
+   //   if ( scroll >= ( boxDrawTop - windowHeight ))        
+              { //object comes into view (scrolling down)
+                if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+              } else { //object goes out of view (scrolling up)
+                if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+              }
+            });
+          }).scroll(); //invoke scroll-handler on page-load
+        });
